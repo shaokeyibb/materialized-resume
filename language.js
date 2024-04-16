a5bd3139894faff45b47e847bcc33fdc8ec2e0d2
HikariLan <hikarilan@minecraft.kim>
2024-04-16 15:27:41 +0800

    feat: top app bar

--- language.js
+++ language.js
@@ -13,12 +13,33 @@ const i18nPages = [
     }
 ]
 
+function getCurrentLanguage(){
+    return (i18nPages.find(it => it.acceptLanguages.includes(new Intl.Locale(navigator.language).language)) ?? i18nPages[0])
+}
+
+function getCurrentPage(){
+    return (i18nPages.find(it => it.page == window.location.pathname))
+}
+
+function redirectToLanagugePage(page){
+    window.location.replace(page.page)
+}
+
+function goToLanguagePage(page){
+    window.location.href = page.page
+}
+
 function changeLanaguge(changeWhenInSubpage) {
-    const page = (i18nPages.find(it => it.acceptLanguages.includes(new Intl.Locale(navigator.language).language)) ?? i18nPages[0]).page // find page
-    if ((changeWhenInSubpage || window.location.pathname == "/") && page != window.location.pathname) { // if in root page but language mismatches
-        window.location.replace(page) // jump to correct page
+    const page = getCurrentLanguage()
+    if ((changeWhenInSubpage || window.location.pathname == "/") && page != getCurrentPage()) {
+        redirectToLanagugePage(page)
     }
 }
+
+export function nextLanguage(){
+    goToLanguagePage(i18nPages[(i18nPages.indexOf(getCurrentPage()) + 1) % i18nPages.length])
+}
+globalThis.nextLanguage = nextLanguage
 
 window.addEventListener('languagechange', () => changeLanaguge(true))
 
